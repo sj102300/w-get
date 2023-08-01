aad6c12e35195a347098ecf851ffe70de152643c
seungJoo <sj10230506@gmail.com>
2023-08-02 01:05:59 +0900

    create calendar page

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -22,7 +22,11 @@ function OneLog(){
 
   return(
     <div className="log">
-      
+      <img alt="플로깅 인증 사진" src="https://www.shutterstock.com/image-photo/plogging-concept-detail-girl-hand-picking-1115182178"/>
+      <div className='log-text'>
+        <h2>2023년 8월 2일</h2>
+        <h3>00h 00m 00s</h3>
+      </div>
     </div>
   )
 }
