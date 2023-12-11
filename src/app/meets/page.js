09cabaf75db6c9ce661222d66665b247e0bb5ed3
seungJoo <sj10230506@gmail.com>
2023-12-11 17:27:09 +0900

    update meets search function

--- src/app/meets/page.js
+++ src/app/meets/page.js
@@ -1,27 +1,36 @@
 import './meets.scss';
 
+import db from '../../utils/database'
 import Link from 'next/link';
 import Image from 'next/image';
 import DetailList from './detailList';
 
 
-function Meets() {
+async function Meets() {
+
+    let detailList = await db.meets.findMany({
+        select: {
+            id: true,
+            title: true,
+            content:true,
+            address:true,
+            daytime: true,
+            current_num: true,
+            max_num: true,
+        }
+    });
+
 
     return (
         <div className='meets'>
             <div className='header'>
                 <div>
-                <Image src="/images/logo.png" alt="logo" width={'30'} height={'30'} />
-                <h2>같이하기</h2>
+                    <Image src="/images/logo.png" alt="logo" width={'30'} height={'30'} />
+                    <h2>같이하기</h2>
                 </div>
                 <button><Link href="/meets/write" >+ 그룹 만들기</Link></button>
             </div>
-            <div className='type-navbar'>
-                <button id="type1" >탐색</button>
-                <button id="type2" >참여</button>
-                <button id="type2" >종료</button>
-            </div>
-            <DetailList />
+            <DetailList detailList={detailList}/>
         </div>
     )
 }
